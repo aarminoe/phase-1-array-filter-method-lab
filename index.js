ee88d770a2af80162a88a50c32120ad010c1d50d
Aaron Noe <aarminoe@gmail.com>
2022-04-27 15:43:53 -0500

    commit tree 1e063a2368486a785dead0190533540089c97078

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+
+
+
+const findMatching = (array, str) => {
+    return array.filter(function (name) {
+        return name === str || name === str.toLowerCase()
+    })
+}
+
+const fuzzyMatch = (array, str) => {
+    return array.filter(function (letter) {
+        return letter.slice(0,1) === str.slice(0,1)
+    })
+}
+
+const matchName = (array, str) => {
+    return array.filter(function (match) {
+        return match.name === str
+    })
+}
